46891ac1185b68222d3a0bb124d33607c0c00e86
Boris Blizniukov <blib@mail.com>
2024-08-24 23:05:13 +0300

    try requestAnimationFrame

--- app/scenes/Logout.tsx
+++ app/scenes/Logout.tsx
@@ -8,14 +8,14 @@ const Logout = () => {
 
   void auth.logout().then(() => {
     if (env.OIDC_LOGOUT_URI) {
-      setTimeout(() => {
+      window.requestAnimationFrame(() => {
         window.location.replace(env.OIDC_LOGOUT_URI);
-      }, 200);
+      });
     }
     if (env.ADFS_URI) {
-      setTimeout(() => {
+      window.requestAnimationFrame(() => {
         window.location.replace(`${env.ADFS_URI}/adfs/oauth2/logout`);
-      }, 200);
+      });
     }
   });
 
